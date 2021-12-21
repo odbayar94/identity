c858d7a651b0e7aef7e8fcb57d7ab5e0f43beb0d
Odbayar <odbayar94@gmail.com>
2021-12-22 00:02:27 +0800

    done

--- endpoints/handler.js
+++ endpoints/handler.js
@@ -1,14 +1,22 @@
 import { User } from "../common/models/User";
 import generateResponse from "../common/response";
 import saveUserInfo from "./services/saveUserInfo";
+import textract from "./services/textract";
+import textractGet from "./services/textractGet";
 
 export async function api(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
   try {
     const functionName = event.pathParameters.functionName;
 
+    if (functionName === "textract") {
+       return await textract({ event });
+     }
     if (functionName === "saveUserInfo") {
       return await saveUserInfo({ event });
+    }
+    if (functionName === "textractGet") {
+      return await textractGet({ event });
     }
 
   } catch (error) {
