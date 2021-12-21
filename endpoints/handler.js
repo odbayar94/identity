66bcb27e4b1a6772697ca46d3748cc688f054bf9
Odbayar <odbayar94@gmail.com>
2021-12-22 00:05:31 +0800

    remove getTextRact request

--- endpoints/handler.js
+++ endpoints/handler.js
@@ -2,7 +2,6 @@ import { User } from "../common/models/User";
 import generateResponse from "../common/response";
 import saveUserInfo from "./services/saveUserInfo";
 import textract from "./services/textract";
-import textractGet from "./services/textractGet";
 
 export async function api(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -14,9 +13,6 @@ export async function api(event, context) {
      }
     if (functionName === "saveUserInfo") {
       return await saveUserInfo({ event });
-    }
-    if (functionName === "textractGet") {
-      return await textractGet({ event });
     }
 
   } catch (error) {
